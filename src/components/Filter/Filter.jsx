110cfda852174aa36a52edd3849e0f6cc40ea4b8
Alexander Kostin <asurv2@gmail.com>
2022-06-21 20:17:24 +0300

    Create Filter.jsx

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,20 @@
+import styles from './styles.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { setFilter, getFilter } from 'redux/contactsSlice';
+
+export default function Filter() {
+    const dispatch = useDispatch();
+    const filter = useSelector(getFilter);
+
+    return (
+        <div className={styles.wrapper}>
+            <p className={styles.text}>Find contacts by name</p>
+            <input
+                className={styles.filter}
+                name="filter"
+                value={filter}
+                onChange={e => dispatch(setFilter(e.target.value))}
+            />
+        </div>
+    );
+}
