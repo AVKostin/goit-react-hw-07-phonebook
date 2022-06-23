cf1d3546bdc7f040fd1677832e139478c1c6b8bc
Alexander Kostin <asurv2@gmail.com>
2022-06-23 18:40:58 +0300

    Update Contacts.jsx

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -3,48 +3,58 @@ import styles from './styles.module.css';
 import { MdOutlineDeleteForever } from 'react-icons/md';
 import { BsTelephone } from 'react-icons/bs';
 import { IoIosContact } from 'react-icons/io';
-import { useDispatch, useSelector } from 'react-redux';
-import { deleteContacts, getFilter, getItems } from 'redux/contactsSlice';
+import { useSelector } from 'react-redux';
+import { getFilter } from 'redux/contactsSlice';
+import {
+    useGetContactsQuery,
+    useDeleteContactMutation,
+} from 'redux/contactsApi';
 
 const Contacts = () => {
-    const contacts = useSelector(getItems);
+    const { data } = useGetContactsQuery();
+
+    const [deleteContact] = useDeleteContactMutation();
+
     const filter = useSelector(getFilter);
-    const dispatch = useDispatch();
 
     const filteredContacts = () => {
-        return contacts.filter(contact =>
-            contact.name.toLowerCase().includes(filter.toLowerCase())
+        return data.filter(contact =>
+            contact.name.toLowerCase().includes(filter.toLowerCase().trim)
         );
     };
-    let rendered = filter === '' ? contacts : filteredContacts();
+    let rendered = filter === '' ? data : filteredContacts();
     return (
-        <ul className={styles.contactsList}>
-            {rendered.map(({ name, id, number }) => (
-                <li className={styles.listItem} key={id} id={id}>
-                    <span className={styles.contactName}>
-                        <IoIosContact size={20} />: {name}{' '}
-                    </span>
-                    <span className={styles.phoneNumber}>
-                        <BsTelephone size={15} />
-                        <span> : </span>
-                        {number}
-                    </span>
+        <>
+            <ul className={styles.contactsList}>
+                {data &&
+                    rendered.map(({ name, id, number }) => (
+                        <li className={styles.listItem} key={id} id={id}>
+                            <span className={styles.contactName}>
+                                <IoIosContact size={20} />: {name}{' '}
+                            </span>
+                            <span className={styles.phoneNumber}>
+                                <BsTelephone size={15} />
+                                <span> : </span>
+                                {number}
+                            </span>
 
-                    <button
-                        className={styles.buttons}
-                        onClick={e =>
-                            dispatch(
-                                deleteContacts(e.currentTarget.parentNode.id)
-                            )
-                        }
-                        title="delete"
-                        aria-label="delete contact button"
-                    >
-                        <MdOutlineDeleteForever size={30} />
-                    </button>
-                </li>
-            ))}
-        </ul>
+                            <button
+                                className={styles.buttons}
+                                onClick={() => deleteContact(id)}
+                                title="delete"
+                                aria-label="delete contact button"
+                            >
+                                <MdOutlineDeleteForever size={30} />
+                            </button>
+                        </li>
+                    ))}
+            </ul>
+            {data && data.length === 0 && (
+                <p style={{ textDecoration: 'underline' }}>
+                    no contacts available
+                </p>
+            )}
+        </>
     );
 };
 
