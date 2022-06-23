88653572d1bcbe4dee5ff8956fac0449c7521754
Alexander Kostin <asurv2@gmail.com>
2022-06-23 18:41:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import 'modern-normalize/modern-normalize.css';
 import './index.css';
 import App from './components/App/App';
 import { Provider } from 'react-redux';
-import { store, persistor } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-                <App />
-            </PersistGate>
+            <App />
         </Provider>
     </React.StrictMode>
 );
