51ce20487d9e97b44d17f7195ccadc66c55033fd
Alexander Kostin <asurv2@gmail.com>
2022-06-23 18:41:14 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,23 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 
-const contactsSlice = createSlice({
+export const contactsSlice = createSlice({
     name: 'contacts',
     initialState: {
-        items: [
-            { id: 'id-1', name: 'Rosie Simpson', number: '+38 095 459 1256' },
-            { id: 'id-2', name: 'Hermione Kline', number: '+38 095 443 8912' },
-            { id: 'id-3', name: 'Eden Clements', number: '+38 095 645 1779' },
-            { id: 'id-4', name: 'Annie Copeland', number: '+38 095 227 9126' },
-        ],
+        items: [],
         filter: '',
     },
     reducers: {
         addContacts(state, action) {
             state.items.push(action.payload);
         },
-        deleteContacts(state, action) {
+        deleteContact(state, action) {
             return {
                 ...state,
                 items: state.items.filter(
@@ -31,17 +24,7 @@ const contactsSlice = createSlice({
     },
 });
 
-const persistConfig = {
-    key: 'root',
-    storage,
-};
-
-export const persistedContactsReducer = persistReducer(
-    persistConfig,
-    contactsSlice.reducer
-);
-
-export const { addContacts, deleteContacts, setFilter } = contactsSlice.actions;
+export const { addContacts, deleteContact, setFilter } = contactsSlice.actions;
 
 export const getItems = state => state.contacts.items;
 
