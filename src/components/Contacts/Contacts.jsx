9a7f1b0805e6baa20273b736b0455337c2faf2b1
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-10-18 21:05:44 +0300

    refactioring

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,29 +1,26 @@
-import styles from './styles.module.css';
-import { useSelector } from 'react-redux';
-import { BsTelephone } from 'react-icons/bs';
-import { IoIosContact } from 'react-icons/io';
-import { getFilter } from 'Redux/contactsSlice';
-import { SpinnerInfinity } from 'spinners-react';
-import { MdOutlineDeleteForever } from 'react-icons/md';
+import { useSelector } from "react-redux";
+import styles from "./styles.module.css";
+import { BsTelephone } from "react-icons/bs";
+import { IoIosContact } from "react-icons/io";
+import { getFilter } from "Redux/contactsSlice";
+import { SpinnerInfinity } from "spinners-react";
+import { MdOutlineDeleteForever } from "react-icons/md";
 import {
     useGetContactsQuery,
     useDeleteContactMutation,
-} from 'Redux/contactsApi';
+} from "Redux/contactsApi";
 
 const Contacts = () => {
     const { data, isFetching } = useGetContactsQuery();
-
     const [deleteContact] = useDeleteContactMutation();
-
     const filter = useSelector(getFilter);
 
     const filteredContacts = () => {
-        return data.filter(contact =>
-            contact.name.toLowerCase().includes(filter.toLowerCase().trim())
+        return data.filter((contact) =>
+            contact.name.toLowerCase().includes(filter.toLowerCase().trim()),
         );
     };
-    let rendered = filter === '' ? data : filteredContacts();
-
+    let rendered = filter === "" ? data : filteredContacts();
     return (
         <>
             <ul className={styles.contactsList}>
@@ -31,7 +28,7 @@ const Contacts = () => {
                     rendered.map(({ name, id, number }) => (
                         <li className={styles.listItem} key={id} id={id}>
                             <span className={styles.contactName}>
-                                <IoIosContact size={20} />: {name}{' '}
+                                <IoIosContact size={20} />: {name}{" "}
                             </span>
                             <span className={styles.phoneNumber}>
                                 <BsTelephone size={15} />
@@ -50,17 +47,16 @@ const Contacts = () => {
                     ))}
             </ul>
             {data && data.length === 0 && (
-                <p style={{ textDecoration: 'underline' }}>
+                <p style={{ textDecoration: "underline" }}>
                     no contacts available
                 </p>
             )}
             {isFetching && (
                 <SpinnerInfinity
-                    style={{ marginTop: '20px', color: 'white' }}
+                    style={{ marginTop: "20px", color: "white" }}
                 />
             )}
         </>
     );
 };
-
 export default Contacts;
