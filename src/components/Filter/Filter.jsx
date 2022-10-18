8e81cf1254d8036f01d49fb2272735ff0e5ba8ba
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-10-18 21:06:46 +0300

    refactioring

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,13 +1,13 @@
-import styles from './styles.module.css';
-import { useSelector, useDispatch } from 'react-redux';
-import { setFilter, getFilter } from 'Redux/contactsSlice';
+import { useSelector, useDispatch } from "react-redux";
+import { setFilter, getFilter } from "Redux/contactsSlice";
+import styles from "./styles.module.css";
 
 export default function Filter() {
     const dispatch = useDispatch();
     const filter = useSelector(getFilter);
 
-    const handleFilterChange = e => dispatch(setFilter(e.currentTarget.value));
-
+    const handleFilterChange = (e) =>
+        dispatch(setFilter(e.currentTarget.value));
     return (
         <div className={styles.wrapper}>
             <p className={styles.text}>Find contacts by name</p>
