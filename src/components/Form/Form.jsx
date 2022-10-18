681b4c2981696f695e9b147d3f084a3f3b3eabb6
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-10-18 21:08:43 +0300

    refactioring

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,31 +1,30 @@
-import PropTypes from 'prop-types';
-import styles from './styles.module.css';
-import { useState, useEffect } from 'react';
-import { TiUserAddOutline } from 'react-icons/ti';
-import Notiflix from 'notiflix';
-import 'react-phone-number-input/style.css';
+import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
+import { TiUserAddOutline } from "react-icons/ti";
+import "react-phone-number-input/style.css";
+import styles from "./styles.module.css";
+import Notiflix from "notiflix";
 import {
     useCreateContactMutation,
     useGetContactsQuery,
-} from 'Redux/contactsApi';
+} from "Redux/contactsApi";
 
 Notiflix.Notify.init({
-    position: 'center-top',
-    width: '400px',
-    fontSize: '18px',
+    position: "center-top",
+    width: "400px",
+    fontSize: "18px",
 });
 
 export default function Form() {
     const [createContact] = useCreateContactMutation();
     const { data } = useGetContactsQuery();
-
-    const [name, setName] = useState('');
-    const [number, setNumber] = useState('');
+    const [name, setName] = useState("");
+    const [number, setNumber] = useState("");
     const [isDisabled, setIsDisabled] = useState(true);
 
     const reset = () => {
-        setName('');
-        setNumber('');
+        setName("");
+        setNumber("");
     };
 
     useEffect(() => {
@@ -34,40 +33,34 @@ export default function Form() {
             : setIsDisabled(true);
     }, [name, number]);
 
-    const handleSubmit = e => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         const contact = {
             name,
             number,
         };
-
         if (name.length === 0)
             return Notiflix.Notify.failure(
-                'Enter valid name / Введите корректное имя'
+                "Enter valid name / Введите корректное имя",
             );
-
         if (number.length !== 13)
             return Notiflix.Notify.failure(
-                'Enter valid 13 digits number / Введите корректный 13-ти значный номер'
+                "Enter valid 13 digits number / Введите корректный 13-ти значный номер",
             );
 
         const contactFinder = data.find(
-            contact => contact.name === name || contact.number === number
+            (contact) => contact.name === name || contact.number === number,
         );
-
         if (contactFinder) {
             // setIsDisabled(true);
             Notiflix.Notify.warning(
-                `${name} ${number} is already in contacts / уже есть в списке ваших контактов.`
+                `${name} ${number} is already in contacts / уже есть в списке ваших контактов.`,
             );
             return;
-            // reset();
         }
         createContact(contact);
-
         reset();
     };
-
     return (
         <form onSubmit={handleSubmit} className={styles.form}>
             <label className={styles.label}>
@@ -79,10 +72,9 @@ export default function Form() {
                     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                     value={name}
-                    onChange={e => setName(e.currentTarget.value)}
+                    onChange={(e) => setName(e.currentTarget.value)}
                 />
             </label>
-
             <label>
                 Number:
                 <input
@@ -93,10 +85,9 @@ export default function Form() {
                     pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                     title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                     value={number}
-                    onChange={e => setNumber(e.currentTarget.value)}
+                    onChange={(e) => setNumber(e.currentTarget.value)}
                 />
             </label>
-
             <button
                 className={styles.submitButton}
                 type="submit"
@@ -116,6 +107,6 @@ Form.propTypes = {
             name: PropTypes.string.isRequired,
             number: PropTypes.string.isRequired,
             id: PropTypes.string.isRequired,
-        })
+        }),
     ),
 };
