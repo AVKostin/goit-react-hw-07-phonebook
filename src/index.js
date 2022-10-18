851f4e2eee2741fa39c993c19c0a32c64228ca49
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-10-18 21:43:57 +0300

    refactioring

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'modern-normalize/modern-normalize.css';
-import './index.css';
-import App from './components/App';
-import { Provider } from 'react-redux';
-import { store } from './Redux/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "modern-normalize/modern-normalize.css";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store";
+import App from "./components/App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
             <App />
         </Provider>
-    </React.StrictMode>
+    </React.StrictMode>,
 );
